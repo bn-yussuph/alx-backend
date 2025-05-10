6a002eabb023ab201bff9c0a8787f5db20972c9f
bn-yussuph <sulehajjaajj@gmail.com>
2025-05-10 03:57:51 +0000

    Amended task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,7 +4,16 @@ Helper function for pagination tuple.
 """
 from typing import Tuple
 
-def index_range(page, page_size) -> Tuple:
+def index_range(page: int, page_size: int) -> Tuple:
+    """A helper function for pagination indices generation
+
+    Args:
+        page (int): th page number
+        page_size (int): the size of the page
+
+    Returns:
+        Tuple: a tuple containing a starting and ending indices of the page
+    """
     end_index = page * page_size
     start_index = end_index - page_size
     return (start_index, end_index)
