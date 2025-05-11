723e1b6dc762341366849d6c7386cdc2dcdca0bc
bn-yussuph <sulehajjaajj@gmail.com>
2025-05-11 23:21:07 +0000

    ammended task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,6 +4,7 @@ Helper function for pagination tuple.
 """
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """A helper function for pagination indices generation
 
