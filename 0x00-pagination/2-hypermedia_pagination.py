3640e46f4c1955f89c0e380dbfd0f1c0378da5f5
bn-yussuph <sulehajjaajj@gmail.com>
2025-05-13 02:12:14 +0000

    return total_pages as an int

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -91,5 +91,5 @@ class Server:
             'data': data,
             'next_page': next_page,
             'prev_page': prev_page,
-            'total_pages': total_pages}
+            'total_pages': int(total_pages)}
         return res
